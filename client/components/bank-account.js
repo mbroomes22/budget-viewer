dfa9804b65cbd7497aa66c75590626d48ecb4341
Michelle <michelle_broomes@yahoo.com>
2020-05-27 11:54:05 -0400

    added bank account viewing options

--- client/components/bank-account.js
+++ client/components/bank-account.js
@@ -4,7 +4,12 @@ export default class Account extends Component {
   render() {
     return (
       <div>
-        <h1>Your Bank Account Details:</h1>
+        <h1>Your Bank Account Details</h1>
+        <div className="acct-choices">
+          <h3>View my Income</h3>
+          <h3>View/set my budget</h3>
+          <h3>View my spending</h3>
+        </div>
       </div>
     )
   }
