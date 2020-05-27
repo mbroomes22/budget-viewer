2ff091368e0b328eae1cac738830ae0696ea061c
Michelle <michelle_broomes@yahoo.com>
2020-05-27 11:07:37 -0400

    successfully installed threejs

--- client/components/about.js
+++ client/components/about.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import * as THREE from 'three'
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Three Budget Viewer</h1>
-    </div>
-  )
+export default class About extends Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>About Three Budget Viewer</h1>
+      </div>
+    )
+  }
 }
-
-export default About
