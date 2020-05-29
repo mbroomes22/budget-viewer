b821ef562812cc647bd12b385652f165dce47f55
Michelle <michelle_broomes@yahoo.com>
2020-05-29 02:51:37 -0400

    changed abt cube

--- client/components/about.js
+++ client/components/about.js
@@ -5,27 +5,61 @@ import * as THREE from 'three'
 export default class About extends Component {
   componentDidMount() {
     var scene = new THREE.Scene()
-    var camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    )
+    var camera = new THREE.PerspectiveCamera(75, 2, 0.1, 100)
     var renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement)
-    var geometry = new THREE.BoxGeometry(1, 1, 1)
-    var material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-    var cube = new THREE.Mesh(geometry, material)
+    // var geometry = new THREE.BoxGeometry(1, 1, 1, 1)
+    //var material = new THREE.MeshBasicMaterial({color: 0x34abeb})
+
+    {
+      const color = 0xffffff
+      const intensity = 1
+      const light = new THREE.DirectionalLight(color, intensity)
+      light.position.set(-1, 2, 4)
+      scene.add(light)
+    }
+
+    //   const boxWidth = 1;
+    // const boxHeight = 1;
+    // const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+    let material = new THREE.MeshPhongMaterial({color: 0xcf9e0c})
+
+    const cube = new THREE.Mesh(geometry, material)
     scene.add(cube)
-    camera.position.z = 5
-    var animate = function() {
+    camera.position.z = 3
+    const animate = function() {
       requestAnimationFrame(animate)
       cube.rotation.x += 0.01
       cube.rotation.y += 0.01
       renderer.render(scene, camera)
     }
     animate()
+    const bgScene = new THREE.Scene()
+    let bgMesh
+    {
+      const loader = new THREE.TextureLoader()
+      const texture = loader.load(
+        'https://threejsfundamentals.org/threejs/resources/images/equirectangularmaps/tears_of_steel_bridge_2k.jpg'
+      )
+      texture.magFilter = THREE.LinearFilter
+      texture.minFilter = THREE.LinearFilter
+
+      const shader = THREE.ShaderLib.equirect
+      material = new THREE.ShaderMaterial({
+        fragmentShader: shader.fragmentShader,
+        vertexShader: shader.vertexShader,
+        uniforms: shader.uniforms,
+        depthWrite: false,
+        side: THREE.BackSide
+      })
+      material.uniforms.tEquirect.value = texture
+      const plane = new THREE.BoxBufferGeometry(2, 2, 2)
+      bgMesh = new THREE.Mesh(plane, material)
+      bgScene.add(bgMesh)
+    }
   }
 
   render() {
