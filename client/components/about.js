589b780284f02c6e9573acd19e91aedfe9c88a94
Michelle <michelle_broomes@yahoo.com>
2020-06-05 17:26:27 -0400

    3d cube changes color on hover

--- client/components/about.js
+++ client/components/about.js
@@ -1,13 +1,28 @@
-import React, {Component} from 'react'
+import React, {Component, useState} from 'react'
 import {Canvas} from 'react-three-fiber'
+
+const Box = () => {
+  const [hovered, setHovered] = useState(false)
+
+  return (
+    <mesh
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshBasicMaterial
+        attach="material"
+        color={hovered ? 'hotpink' : 'lightblue'}
+      />
+    </mesh>
+  )
+}
 
 export default class About extends Component {
   render() {
     return (
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach="geometry" arcs={[1, 1, 1]} />
-        </mesh>
+        <Box />
       </Canvas>
     )
   }
