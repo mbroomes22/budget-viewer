f02a87e3f2115247a5f1a45062d9a788c7ab0061
Michelle <michelle_broomes@yahoo.com>
2020-06-09 11:50:43 -0400

    calculator works

--- client/components/currency/calculator.js
+++ client/components/currency/calculator.js
@@ -1,78 +1,116 @@
 import React, {Component} from 'react'
 
 export default class Calculator extends Component {
+  constructor() {
+    super()
+    this.state = {
+      equation: '',
+      result: 0
+    }
+  }
+
+  onClick = event => {
+    let equation = this.state.equation
+    const pressedButton = event.target.innerHTML
+    if ((pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.')
+      equation += pressedButton
+    else if (['+', '-', '*', '/', '%'].indexOf(pressedButton) !== -1)
+      equation += ' ' + pressedButton + ' '
+    else if (pressedButton === '=') {
+      try {
+        const evalResult = eval(equation)
+        const result = Number.isInteger(evalResult)
+          ? evalResult
+          : evalResult.toFixed(2)
+        this.setState({result})
+      } catch (error) {
+        alert('Invalid Mathematical Equation')
+      }
+    } else {
+      equation = equation.trim()
+      equation = equation.substr(0, equation.length - 1)
+    }
+
+    this.setState({equation: equation})
+  }
+
+  clear = evt => {
+    evt.preventDefault()
+    this.setState({equation: '', result: 0})
+  }
+
   render() {
     return (
       <main className="calculator">
         <section className="screen">
-          <div className="result-screen">3124</div>
-          <div className="computation-screen">3000 + 100 + 20 + 4</div>
+          <div className="result-screen">{this.state.result}</div>
+          <div className="computation-screen">{this.state.equation}</div>
         </section>
         <section className="keypad">
           <div className="keypad-row">
-            <button type="button" className="btn">
+            <button type="button" onClick={this.clear} className="btn">
               C
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               &larr;
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               %
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               /
             </button>
           </div>
           <div className="keypad-row">
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               9
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               8
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               7
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               *
             </button>
           </div>
           <div className="keypad-row">
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               6
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               5
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               4
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               -
             </button>
           </div>
           <div className="keypad-row">
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               3
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               2
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               1
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               +
             </button>
           </div>
           <div className="keypad-row">
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               0
             </button>
-            <button type="button" className="btn">
+            <button type="button" onClick={this.onClick} className="btn">
               .
             </button>
-            <button type="button" className="btn-large">
+            <button type="button" onClick={this.onClick} className="btn-large">
               =
             </button>
             {/* <button type="button" className="btn"></button> */}
