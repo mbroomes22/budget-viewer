40d16a7ceaa71bacb20095d99ff1fa77757b91f2
Michelle <michelle_broomes@yahoo.com>
2020-06-09 17:28:59 -0400

    gave 3d bg to coming soon pgs

--- client/components/portal-views/income-portal.js
+++ client/components/portal-views/income-portal.js
@@ -1,9 +1,71 @@
-import React, {Component} from 'react'
+import React, {Component, useState, useRef, useEffect} from 'react'
 import {Link} from 'react-router-dom'
-// import * as THREE from '../build/three.module.js';
-// import { VRButton } from './jsm/webxr/VRButton.js';
-//https://github.com/mrdoob/three.js/blob/master/examples/webxr_vr_panorama_depth.html
-//https://threejs.org/examples/#webxr_vr_panorama
+import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import {Canvas, extend, useThree, useFrame} from 'react-three-fiber'
+import {useSpring, a} from 'react-spring/three'
+
+extend({OrbitControls})
+
+const AirCraft = () => {
+  const [model, setModel] = useState()
+  useEffect(() => {
+    new GLTFLoader().load('../../public/3d_files/airplane/scene.gltf', setModel)
+  })
+
+  return model ? <primitive object={model.scene} /> : null
+}
+
+const Controls = () => {
+  const {camera, gl} = useThree()
+  const orbitRef = useRef()
+
+  useFrame(() => {
+    orbitRef.current.update()
+  })
+
+  return (
+    <orbitControls
+      autoRotate
+      maxPolarAngle={Math.PI / 3}
+      minPolarAngle={Math.PI / 3}
+      args={[camera, gl.domElement]}
+      ref={orbitRef}
+    />
+  )
+}
+
+const Plane = () => (
+  <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
+    <planeBufferGeometry attach="geometry" args={[100, 100]} />
+    <meshPhysicalMaterial attach="material" color="rgb(57, 15, 181)" />
+  </mesh>
+)
+
+const Box = () => {
+  const [hovered, setHovered] = useState(false)
+  const [active, setActive] = useState(false)
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? '#ffaac3' : '#b063c5'
+  })
+
+  return (
+    <a.mesh
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}
+      castShadow
+    >
+      <ambientLight />
+      <spotLight position={[0, 5, 10]} penumbra={1} castShadow />
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <a.meshPhysicalMaterial attach="material" color={props.color} />
+    </a.mesh>
+  )
+}
 
 export default class Income extends Component {
   constructor() {
@@ -41,20 +103,44 @@ export default class Income extends Component {
   render() {
     this.counter()
     return (
-      <div>
-        <h2>Income</h2>
-        <h3>
-          Enter a new dimension to visualize your budget like never before
-        </h3>
-        <div className="middle">
-          <h1>COMING SOON</h1>
-          <hr />
-          <p id="countdown">{this.state.display}</p>
-          <p>
-            Return to <Link to="/account">Account Details</Link>
-          </p>
+      <>
+        <Canvas
+          camera={{position: [0, 0, 5]}}
+          onCreated={({gl}) => {
+            gl.shadowMap.enabled = true
+            gl.shadowMap.type = THREE.PCFSoftShadowMap
+          }}
+        >
+          <fog attach="fog" args={['rgb(57, 15, 181)', 5, 15]} />
+          <Controls />
+          <Box />
+          <Plane />
+          <AirCraft />
+        </Canvas>
+        <div className="three-d-soon">
+          <h2>Income</h2>
+          <h3>
+            Enter a new dimension to visualize your budget like never before
+          </h3>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <div className="middle">
+            <h1>COMING SOON</h1>
+            <hr />
+            <p id="countdown">{this.state.display}</p>
+            <p>
+              Return to <Link to="/account">Account Details</Link>
+            </p>
+          </div>
         </div>
-      </div>
+      </>
     )
   }
 }
