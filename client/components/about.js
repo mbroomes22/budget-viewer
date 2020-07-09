1ebefbb95a30cf4956de50fff990dcd4b3821120
Michelle <michelle_broomes@yahoo.com>
2020-07-09 01:42:07 -0400

    3d text probs

--- client/components/about.js
+++ client/components/about.js
@@ -1,112 +1,198 @@
-import React, {useRef, useState} from 'react'
-//R3F
-import {Canvas, useFrame} from 'react-three-fiber'
-// Deai - R3F
-import {softShadows, MeshWobbleMaterial, OrbitControls} from 'drei'
-//Components
-// import Header from "./components/header";
-// Styles
-// import "./App.scss";
-// React Spring
-import {useSpring, a} from 'react-spring/three'
+import ReactDOM from 'react-dom'
+import React, {Suspense, useEffect, useRef, useMemo} from 'react'
+import {Canvas, Dom, useLoader, useFrame} from 'react-three-fiber'
+import {TextureLoader, LinearFilter} from 'three'
+import lerp from 'lerp'
+import {Text, MultilineText} from './assets/Text'
+// import Diamonds from "./diamonds/Diamonds"
+import Plane from './assets/Plane'
+import {Block, useBlock} from './blocks'
+import state from './storie'
+import {HTML} from 'drei'
+// import "./styles.css"
 
-// soft Shadows
-softShadows()
+function Startup() {
+  const ref = useRef()
+  useFrame(
+    () =>
+      (ref.current.material.opacity = lerp(
+        ref.current.material.opacity,
+        0,
+        0.025
+      ))
+  )
+  return (
+    <Plane
+      ref={ref}
+      color="#0e0e0f"
+      position={[0, 0, 200]}
+      scale={[100, 100, 1]}
+    />
+  )
+}
 
-const SpinningMesh = ({position, color, speed, args}) => {
-  //ref to target the mesh
-  const mesh = useRef()
+const Paragraph = ({image, index, offset, factor, header, aspect, text}) => {
+  const {contentMaxWidth: w, canvasWidth, margin, mobile} = useBlock()
+  const size = aspect < 1 && !mobile ? 0.65 : 1
+  const alignRight = (canvasWidth - w * size - margin) / 2
+  const pixelWidth = w * state.zoom * size
+  const left = !(index % 2)
+  const color = index % 2 ? '#D40749' : '#2FE8C3'
+  return (
+    <Block factor={factor} offset={offset}>
+      <group position={[left ? -alignRight : alignRight, 0, 0]}>
+        <Plane
+          map={image}
+          args={[1, 1, 32, 32]}
+          shift={75}
+          size={size}
+          aspect={aspect}
+          scale={[w * size, w * size / aspect, 1]}
+          frustumCulled={false}
+        />
+        <HTML
+          style={{
+            width: pixelWidth / (mobile ? 1 : 2),
+            textAlign: left ? 'left' : 'right'
+          }}
+          position={[
+            left || mobile ? -w * size / 2 : 0,
+            -w * size / 2 / aspect - 0.4,
+            1
+          ]}
+        >
+          <div tabIndex={index}>{text}</div>
+        </HTML>
+        <Text
+          left={left}
+          right={!left}
+          size={w * 0.04}
+          color={color}
+          top
+          position={[
+            (left ? -w : w) * size / 2,
+            w * size / aspect / 2 + 0.5,
+            -1
+          ]}
+        >
+          {header}
+        </Text>
+        <Block factor={0.2}>
+          <Text
+            opacity={0.5}
+            size={w * 0.1}
+            color="#1A1E2A"
+            position={[
+              (left ? w : -w) / 2 * size,
+              w * size / aspect / 1.5,
+              -10
+            ]}
+          >
+            {'0' + (index + 1)}
+          </Text>
+        </Block>
+      </group>
+    </Block>
+  )
+}
 
-  //useFrame allows us to re-render/update rotation on each frame
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
-
-  //Basic expand state
-  const [expand, setExpand] = useState(false)
-  // React spring expand animation
-  const props = useSpring({
-    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1]
-  })
+function Content() {
+  const images = useLoader(
+    TextureLoader,
+    state.paragraphs.map(({image}) => image)
+  )
+  useMemo(() => images.forEach(texture => (texture.minFilter = LinearFilter)), [
+    images
+  ])
+  const {contentMaxWidth: w, canvasWidth, canvasHeight, mobile} = useBlock()
   return (
     <>
-      <a.mesh
-        position={position}
-        ref={mesh}
-        onClick={() => setExpand(!expand)}
-        scale={props.scale}
-        castShadow
-      >
-        <boxBufferGeometry attach="geometry" args={args} />
-        <MeshWobbleMaterial
-          color={color}
-          speed={speed}
-          attach="material"
-          factor={0.6}
+      <Block factor={1} offset={0}>
+        <Block factor={1.2}>
+          <Text
+            left
+            size={w * 0.08}
+            position={[-w / 3.2, 0.5, -1]}
+            color="#d40749"
+          >
+            MOKSHA
+          </Text>
+        </Block>
+        <Block factor={1.0}>
+          <HTML position={[-w / 3.2, -w * 0.08 + 0.25, -1]}>
+            It was the year 2076.{mobile ? <br /> : ' '}The substance had
+            arrived.
+          </HTML>
+        </Block>
+      </Block>
+      <Block factor={1.2} offset={5.7}>
+        <MultilineText
+          top
+          left
+          size={w * 0.15}
+          lineHeight={w / 5}
+          position={[-w / 3.5, 0, -1]}
+          color="#2fe8c3"
+          text={'four\nzero\nzero'}
         />
-      </a.mesh>
-
-      {/* //Using Drei box if you want */}
-      {/* <Box {...props} ref={mesh} castShadow>
-      <MeshWobbleMaterial
-        {...props}
-        attach='material'
-        factor={0.6}
-        Speed={1}
-      />
-    </Box> */}
+      </Block>
+      {state.paragraphs.map((props, index) => (
+        <Paragraph key={index} index={index} {...props} image={images[index]} />
+      ))}
+      {state.stripes.map(({offset, color, height}, index) => (
+        <Block key={index} factor={-1.5} offset={offset}>
+          <Plane
+            args={[50, height, 32, 32]}
+            shift={-4}
+            color={color}
+            rotation={[0, 0, Math.PI / 8]}
+            position={[0, 0, -10]}
+          />
+        </Block>
+      ))}
+      <Block factor={1.25} offset={8}>
+        <HTML
+          className="bottom-left"
+          position={[-canvasWidth / 2, -canvasHeight / 2, 0]}
+        >
+          Culture is not your friend.
+        </HTML>
+      </Block>
     </>
   )
 }
 
-const About = () => {
+function About() {
+  const scrollArea = useRef()
+  const onScroll = e => (state.top.current = e.target.scrollTop)
+  useEffect(() => void onScroll({target: scrollArea.current}), [])
   return (
     <>
-      {/* <Header /> */}
-      {/* Our Scene & Camera is already built into our canvas */}
       <Canvas
-        colorManagement
-        shadowMap
-        camera={{position: [-5, 2, 10], fov: 60}}
+        concurrent
+        pixelRatio={1}
+        orthographic
+        camera={{zoom: state.zoom, position: [0, 0, 500]}}
       >
-        {/* This light makes things look pretty */}
-        <ambientLight intensity={0.3} />
-        {/* Our main source of light, also casting our shadow */}
-        <directionalLight
-          castShadow
-          position={[0, 10, 0]}
-          intensity={1.5}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-          shadow-camera-far={50}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
-        />
-        {/* A light to help illumnate the spinning boxes */}
-        <pointLight position={[-10, 0, -20]} intensity={0.5} />
-        <pointLight position={[0, -10, 0]} intensity={1.5} />
-        <group>
-          {/* This mesh is the plane (The floor) */}
-          <mesh
-            rotation={[-Math.PI / 2, 0, 0]}
-            position={[0, -3, 0]}
-            receiveShadow
-          >
-            <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            <shadowMaterial attach="material" opacity={0.3} />
-          </mesh>
-          <SpinningMesh
-            position={[0, 1, 0]}
-            color="lightblue"
-            args={[3, 2, 1]}
-            speed={2}
-          />
-          <SpinningMesh position={[-2, 1, -5]} color="pink" speed={6} />
-          <SpinningMesh position={[5, 1, -2]} color="pink" speed={6} />
-        </group>
-        {/* Allows us to move the canvas around for different prespectives */}
-        <OrbitControls />
+        <Suspense
+          fallback={<HTML center className="loading" children="Loading..." />}
+        >
+          <Content />
+          {/* <Diamonds /> */}
+          <Startup />
+        </Suspense>
       </Canvas>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        {new Array(state.sections)
+          .fill()
+          .map((_, index) => (
+            <div
+              key={index}
+              id={'0' + index}
+              style={{height: `${state.pages / state.sections * 100}vh`}}
+            />
+          ))}
+      </div>
     </>
   )
 }
--- client/components/blocks.js
+++ client/components/blocks.js
@@ -1,19 +1,7 @@
-import React, {createContext, useRef, useContext, createRef} from 'react'
+import React, {createContext, useRef, useContext} from 'react'
 import {useFrame, useThree} from 'react-three-fiber'
 import lerp from 'lerp'
-// import state from "./store"
-
-const state = {
-  sections: 3,
-  pages: 3,
-  zoom: 75,
-  images: [
-    '/photo-1548191265-cc70d3d45ba1.jpeg',
-    '/photo-1519608487953-e999c86e7455.jpeg',
-    '/photo-1533577116850-9cc66cad8a9b.jpeg'
-  ],
-  top: createRef()
-}
+import state from './storie'
 
 const offsetContext = createContext(0)
 
@@ -39,11 +27,11 @@ function useBlock() {
   const {sections, pages, zoom} = state
   const {size, viewport} = useThree()
   const offset = useContext(offsetContext)
-  const viewportWidth = 1200
-  const viewportHeight = 2000
+  const viewportWidth = viewport.width
+  const viewportHeight = viewport.height
   const canvasWidth = viewportWidth / zoom
   const canvasHeight = viewportHeight / zoom
-  const mobile = window.screen.availWidth < 700
+  const mobile = size.width < 700
   const margin = canvasWidth * (mobile ? 0.2 : 0.1)
   const contentMaxWidth = canvasWidth * (mobile ? 0.8 : 0.6)
   const sectionHeight = canvasHeight * ((pages - 1) / (sections - 1))
