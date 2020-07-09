15f080f8c7c662d1d529c193061285a00b47c9c2
Michelle <michelle_broomes@yahoo.com>
2020-07-08 21:46:53 -0400

    3 wobbly obj

--- client/components/about.js
+++ client/components/about.js
@@ -1,118 +1,112 @@
-import ReactDOM from 'react-dom'
-import lerp from 'lerp'
-import React, {useRef, useEffect, createRef} from 'react'
-import {Canvas, useFrame, Dom} from 'react-three-fiber'
-import {Block, useBlock} from './blocks'
+import React, {useRef, useState} from 'react'
+//R3F
+import {Canvas, useFrame} from 'react-three-fiber'
+// Deai - R3F
+import {softShadows, MeshWobbleMaterial, OrbitControls} from 'drei'
+//Components
+// import Header from "./components/header";
+// Styles
+// import "./App.scss";
+// React Spring
+import {useSpring, a} from 'react-spring/three'
 
-const state = {
-  sections: 3,
-  pages: 3,
-  zoom: 75,
-  top: createRef()
-}
+// soft Shadows
+softShadows()
 
-function Plane({color = 'white', ...props}) {
-  return (
-    <mesh {...props}>
-      <planeBufferGeometry attach="geometry" />
-      <meshBasicMaterial attach="material" color={color} />
-    </mesh>
-  )
-}
+const SpinningMesh = ({position, color, speed, args}) => {
+  //ref to target the mesh
+  const mesh = useRef()
 
-function Cross() {
-  const ref = useRef()
-  const {viewportHeight} = useBlock()
-  useFrame(() => {
-    const curTop = state.top.current
-    const curY = ref.current.rotation.z
-    const nextY = curTop / ((state.pages - 1) * viewportHeight) * Math.PI
-    ref.current.rotation.z = lerp(curY, nextY, 0.1)
+  //useFrame allows us to re-render/update rotation on each frame
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+
+  //Basic expand state
+  const [expand, setExpand] = useState(false)
+  // React spring expand animation
+  const props = useSpring({
+    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1]
   })
   return (
-    <group ref={ref} scale={[2, 2, 2]}>
-      <Plane scale={[1, 0.2, 0.2]} color="#e2bfca" />
-      <Plane scale={[0.2, 1, 0.2]} color="#e2bfca" />
-    </group>
-  )
-}
+    <>
+      <a.mesh
+        position={position}
+        ref={mesh}
+        onClick={() => setExpand(!expand)}
+        scale={props.scale}
+        castShadow
+      >
+        <boxBufferGeometry attach="geometry" args={args} />
+        <MeshWobbleMaterial
+          color={color}
+          speed={speed}
+          attach="material"
+          factor={0.6}
+        />
+      </a.mesh>
 
-function Content({left, children}) {
-  const {contentMaxWidth, canvasWidth, margin} = useBlock()
-  const aspect = 1.75
-  const alignRight = (canvasWidth - contentMaxWidth - margin) / 2
-  return (
-    <group position={[alignRight * (left ? -1 : 1), 0, 0]}>
-      <Plane
-        scale={[contentMaxWidth, contentMaxWidth / aspect, 1]}
-        color="#bfe2ca"
+      {/* //Using Drei box if you want */}
+      {/* <Box {...props} ref={mesh} castShadow>
+      <MeshWobbleMaterial
+        {...props}
+        attach='material'
+        factor={0.6}
+        Speed={1}
       />
-      {children}
-    </group>
+    </Box> */}
+    </>
   )
 }
 
-function Stripe() {
-  const {contentMaxWidth} = useBlock()
-  return (
-    <Plane
-      scale={[100, contentMaxWidth, 1]}
-      rotation={[0, 0, Math.PI / 4]}
-      position={[0, 0, -1]}
-      color="#e3f6f5"
-    />
-  )
-}
-
-function About() {
-  const scrollArea = useRef()
-  const {contentMaxWidth, mobile} = useBlock()
-  const aspect = 1.75
-  const pixelWidth = contentMaxWidth * state.zoom
-  const onScroll = e => {
-    state.top.current = e.target.scrollTop
-  }
-  useEffect(() => void onScroll({target: scrollArea.current}), [])
+const About = () => {
   return (
     <>
-      <h3>About Us</h3>
-      <Canvas orthographic camera={{zoom: state.zoom, position: [0, 0, 500]}}>
-        {/* First section */}
-        <Block factor={1.5} offset={0}>
-          <Content left>
-            <Dom
-              style={{width: pixelWidth / (mobile ? 1 : 2), textAlign: 'left'}}
-              position={[
-                -contentMaxWidth / 2,
-                -contentMaxWidth / 2 / aspect - 0.4,
-                1
-              ]}
-            >
-              The substance can take you to heaven but it can also take you to
-              hell.
-            </Dom>
-          </Content>
-        </Block>
-        {/* Second section */}
-        <Block factor={2.0} offset={1}>
-          <Content />
-        </Block>
-        {/* Stripe */}
-        <Block factor={-1.0} offset={1}>
-          <Stripe />
-        </Block>
-        {/* Last section */}
-        <Block factor={1.5} offset={2}>
-          <Content left>
-            <Block factor={-0.5}>
-              <Cross />
-            </Block>
-          </Content>
-        </Block>
+      {/* <Header /> */}
+      {/* Our Scene & Camera is already built into our canvas */}
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{position: [-5, 2, 10], fov: 60}}
+      >
+        {/* This light makes things look pretty */}
+        <ambientLight intensity={0.3} />
+        {/* Our main source of light, also casting our shadow */}
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        {/* A light to help illumnate the spinning boxes */}
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <group>
+          {/* This mesh is the plane (The floor) */}
+          <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+            receiveShadow
+          >
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
+          <SpinningMesh
+            position={[0, 1, 0]}
+            color="lightblue"
+            args={[3, 2, 1]}
+            speed={2}
+          />
+          <SpinningMesh position={[-2, 1, -5]} color="pink" speed={6} />
+          <SpinningMesh position={[5, 1, -2]} color="pink" speed={6} />
+        </group>
+        {/* Allows us to move the canvas around for different prespectives */}
+        <OrbitControls />
       </Canvas>
-      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
-        <div style={{height: `${state.pages * 100}vh`}} />
-      </div>
     </>
   )
 }
