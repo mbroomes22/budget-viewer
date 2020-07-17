e17bc8e8b27a9f302ed164fc9574a864ae028c46
Michelle <michelle_broomes@yahoo.com>
2020-07-17 15:39:46 -0400

    default about

--- client/components/about.js
+++ client/components/about.js
@@ -1,25 +1,46 @@
 import ReactDOM from 'react-dom'
-import React, {Suspense, useEffect, useRef, useMemo} from 'react'
-import {Canvas, Dom, useLoader, useFrame} from 'react-three-fiber'
-import {TextureLoader, LinearFilter} from 'three'
-import lerp from 'lerp'
-// import {Text, MultilineText} from './assets/Text'
-// import Diamonds from "./diamonds/Diamonds"
-// import Plane from './assets/Plane'
-import {Block, useBlock} from './blocks'
-import state from './storie'
-import {HTML} from 'drei'
-// import "./styles.css"
+import React, {useRef, useState} from 'react'
+import {Canvas, useFrame} from 'react-three-fiber'
+
+const Box = props => {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={e => setActive(!active)}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial
+        attach="material"
+        color={hovered ? 'hotpink' : 'orange'}
+      />
+    </mesh>
+  )
+}
 
 function About() {
   return (
     <>
-      <Canvas
-        concurrent
-        pixelRatio={1}
-        orthographic
-        camera={{zoom: state.zoom, position: [0, 0, 500]}}
-      />
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+      <p>hello</p>
     </>
   )
 }
--- client/components/financials/stocks.js
+++ client/components/financials/stocks.js
@@ -4,7 +4,10 @@ export default class Stocks extends Component {
   render() {
     return (
       <div>
-        <h3>Daily Stock info</h3>
+        <img
+          src="https://i.insider.com/5a971dbdaae60520008b45b4?width=1100&format=jpeg&auto=webp"
+          width="100%"
+        />
       </div>
     )
   }
